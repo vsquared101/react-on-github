7eaa8454f16e32916d23f55c92d3121a712f96af
Vivek Varma <vsquared101@gmail.com>
2020-03-25 19:05:17 +0530

    Add bala again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import bala from './bala.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={bala} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to N Squares!
         </p>
